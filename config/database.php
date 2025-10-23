50bc813038e38545f035be926803676f6d1a06ef
AhmedRHegab <112779709+AhmedRHegab@users.noreply.github.com>
2025-10-23 14:28:22 +0200

    remove db url from code

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('mysql://root:qPSbUsxPZPpVcISiQwcNBtLnFsPdmMqd@mysql.railway.internal:3306/railway'),
+            //'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
