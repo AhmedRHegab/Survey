166451cb989fbd5979dc349283bd61bf3603f336
AhmedRHegab <112779709+AhmedRHegab@users.noreply.github.com>
2025-10-23 14:22:24 +0200

    set db url from code

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+            'url' => env('mysql://root:qPSbUsxPZPpVcISiQwcNBtLnFsPdmMqd@mysql.railway.internal:3306/railway'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
